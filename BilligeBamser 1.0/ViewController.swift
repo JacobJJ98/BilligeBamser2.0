d1fd6ed6d8b344812e8f71a8957008191a219354
Jacob <jj>
2019-10-09 13:50:37 +0200

    Initial Commit

---
+++ BilligeBamser 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BilligeBamser 1.0
+//
+//  Created by Jacob Jørgensen on 09/10/2019.
+//  Copyright © 2019 Jacob Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
