02109d26f9603c213a2267655d44cbb9f862e853
Jacob <jj>
2019-10-16 21:28:13 +0200

    Lavet så man nu kan prøve at loade kortet med de hentede barer. Så kan placeringen ændres inde på Firebase og så kan man gå til udforsk og tilbage igen for at se placeringen er opdateret. Det er bare lavet for at teste hvordan det fungerer med DB osv

---
+++ BilligeBamser 1.0/MereViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MereViewController.swift
+//  BilligeBamser 1.0
+//
+//  Created by Jacob Jørgensen on 16/10/2019.
+//  Copyright © 2019 Jacob Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class MereViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MERE VIEW!!")
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("APPEAR MERE!!")
+    }
+    
+
+
+}
