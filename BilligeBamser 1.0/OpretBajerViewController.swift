2b2b2e59adf4e8fc85e18100707dcd634ea39e4e
Jacob <jj>
2019-10-19 17:02:44 +0200

    Nu skal det næsten bare smides på Firebase. Der skal dog lige ændres på rækkefølgen i forhold til verificering, så den tjekker oppefra og ned!!

--- BilligeBamser 1.0/OpretBajerViewController.swift
+++ BilligeBamser 1.0/OpretBajerViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
-class OpretBajerViewController: UIViewController {
+class OpretBajerViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
     
-
+    var pris : Int? = nil
+    var cor : CLLocationCoordinate2D?
+    var midlerRyg : Bool? = nil
+    var navn : String? = nil
+    @IBOutlet weak var navnAfvist: UILabel!
+    @IBOutlet weak var prisAfvist: UILabel!
+    @IBOutlet weak var adrAfvist: UILabel!
+    
+    @IBOutlet weak var nuværendeLokationStatus: UISwitch!
     @IBOutlet weak var barNavn: UITextField!
     @IBOutlet weak var flaskePris: UITextField!
     @IBOutlet weak var rygningTilladt: UISwitch!
@@ -27,10 +37,22 @@ class OpretBajerViewController: UIViewController {
     }
     
     @IBAction func opretBar(_ sender: UIButton) {
-    }
+        fjernAfvistLabels()
+        print("opretBar er trykket")
+        // kaldes for at finde lokationen og derefter kaldes der en ny metode inde fra denne!
+        self.findLokation()
+        
+        }
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func  fjernAfvistLabels() -> Void {
+        prisAfvist.isHidden = true
+        navnAfvist.isHidden = true
+        adrAfvist.isHidden = true
     }
     
    
@@ -43,6 +65,105 @@ class OpretBajerViewController: UIViewController {
     func performSegueToReturnBack()  {
         
             self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func opretBarenEfterKoordinat() -> Void {
+        if let navnmidler = barNavn.text {
+            guard navnmidler.count > 0 else {
+                navnAfvist.isHidden = false
+                return
+            }
+            //sætter navnet til klassevariablen!
+            navn = navnmidler
+        }
+        if let prismidler = flaskePris.text {
+            guard prismidler.count > 0 else {
+                prisAfvist.isHidden = false
+                return
+            }
+                
+            if let prisSomInt = Int(prismidler) {
+                pris = prisSomInt
+            }
+        }
+        print("INDE I OPRET EFTER KOR")
+        if let kor = cor {
+            if let navn_ = navn {
+                if let pris_ = pris {
+                        print("BAREN KAN NU OPRETTES MED DISSE VÆRDIER: ")
+                        print(kor.latitude)
+                        print(kor.longitude)
+                        print(navn_)
+                        print(pris_)
+                        print(rygningTilladt.isOn)
+                    
+                    
+                }
+            }
+        }
+    }
+    
+     func findLokation() -> Void {
+        print("inde i findLokation")
+        // find ud af om det skal være nuværende lokation eller adresse
+            if nuværendeLokationStatus.isOn {
+                
+                
+                  //Resterende sørger for at sætte current location som der hvor Map starter!!
+                  if CLLocationManager.locationServicesEnabled() {
+                    locationManager.delegate = self
+                      if let lokationen = locationManager.location {
+                        print("LOKATIONEN ER: ")
+                        print(lokationen.coordinate.latitude)
+                        print(lokationen.coordinate.longitude)
+                        // sætter cor til være den nuværende lokation!
+                        cor = lokationen.coordinate
+                        self.opretBarenEfterKoordinat()
+                      } else {
+                        // TODO:  lav en allert eller noget NICE
+                        print("DU SKAL ACCEPTERE BRUG AF LOKATION!!!")
+                    }
+            }
+         
+            
+        } else {
+                
+                if let adrmidler = adresse.text {
+                    guard adrmidler.count > 0 else {
+                        adrAfvist.isHidden = false
+                        return
+                    }
+                }
+                let address = "Toftebakken 15, 3790 Hasle"
+
+                let geoCoder = CLGeocoder()
+                // if let adress = adresse.text {
+                    
+                    geoCoder.geocodeAddressString(address) { (placemarks, error) in
+                        guard let placemarks = placemarks, let location = placemarks.first?.location
+                         else {
+                            // handle no location found
+                            // tænker der skal komme en alert omkring det skal indtastes anderledes eller brug nuværende loka!!
+                            
+                        print("Lokationen findes ikke!")
+                            return
+                        }
+                        
+
+                      //  print("DEN INDTASTEDE LOKATION ER: ")
+                     //   print(location.coordinate.latitude)
+                     //   print(location.coordinate.longitude)
+                        
+                        self.cor = location.coordinate
+                        self.opretBarenEfterKoordinat()
+                        
+                    }
+                    
+               // }
+                
+            
+          }
         
     }
    
