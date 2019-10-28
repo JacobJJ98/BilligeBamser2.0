bf27eccfe3ee68456931035525107a371fbbab7d
Jacob <jj>
2019-10-28 21:20:28 +0100

    Der er kontakt til firebase nu, men jeg er IKKE færdig.

--- BilligeBamser 1.0/Start/OpretBrugerViewController.swift
+++ BilligeBamser 1.0/Start/OpretBrugerViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import FirebaseAuth
 import FirebaseFirestore
+import SVProgressHUD
 
 class OpretBrugerViewController: UIViewController {
-
+    var db: Firestore!
     
-    var fornavnString = ""
-    var efternavnString = ""
+    var fornavnString = "TEST"
+    var efternavnString = "TEST"
     var mailString = ""
     var kodeString = ""
     
@@ -31,6 +32,7 @@ class OpretBrugerViewController: UIViewController {
     
     
     @IBAction func onOpretBruger(_ sender: UIButton) {
+        SVProgressHUD.show()
         if(self.validerFelterne()) {
             self.opretBrugerPaaFirebase()
             self.gaaTilMenu()
@@ -39,14 +41,76 @@ class OpretBrugerViewController: UIViewController {
     }
     func validerFelterne() -> Bool {
         // TODO: tjek at der er indtastet noget i navn/efternavn. Tjek også om det er en gyldig mail (validator) og tjek at kode er over en vis længde
+        if let mmaaiil = mail.text {
+            mailString = mmaaiil
+        }
+        if let kkooddee = kode.text {
+            kodeString = kkooddee
+        }
         return true
     }
     func opretBrugerPaaFirebase() -> Void {
         //TODO: opret i authentication og derefter i fireStore. tjek her at document ID ikke findes i forvejen, fordi så findes brugeren. og hvad hvis man bruger den sammen mail to gange?
         
-      //  Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>) { (ress, err) in
-     //       <#code#>
-     //   }
+        Auth.auth().createUser(withEmail: mailString, password: kodeString) { (ress, err) in
+           if err != nil {
+            print("OPRETTELSE FEJLET!")
+            if err.unsafelyUnwrapped.localizedDescription == "The email address is already in use by another account." {
+                SVProgressHUD.showError(withStatus: "Mailen findes allerede")
+                print(err.unsafelyUnwrapped.localizedDescription)
+                SVProgressHUD.dismiss(withDelay: 2)
+            } else {
+                SVProgressHUD.showError(withStatus: "Der opstod en fejl")
+                print("EFTER IF'en!!")
+                SVProgressHUD.dismiss(withDelay: 2)
+            }
+            if let user = Auth.auth().currentUser {
+                print (user.providerID)
+            }
+           } else {
+            print("BRUGEREN BLEV OPRETTET!")
+            print(ress.debugDescription)
+            print("BRUGEREN SOM ER LOGGET IND: ")
+            
+            if let user = Auth.auth().currentUser {
+                print(user.uid)
+            }
+            
+            self.db = Firestore.firestore()
+            print("INDE I FIREstore tilføj")
+            var favo: [String] = []
+            var ref: DocumentReference? = nil
+            if let user = Auth.auth().currentUser {
+                
+                self.db.collection("Bruger").document(user.uid).setData([
+                    "Fornavn": self.fornavnString,
+                    "Efternavn": self.efternavnString,
+                                "Favoritsteder": favo
+                            ]) { err in
+                                if let err = err {
+                                    print("FEJL NÅR BRUGER SKULLE I DB")
+                                    SVProgressHUD.showError(withStatus: "Der opstod en fejl")
+                                    SVProgressHUD.dismiss(withDelay: 2)
+                                } else {
+                                  print("SUCCES MED AT TILFØJE BRUGER I DB")
+                                    SVProgressHUD.showSuccess(withStatus: "Bruger oprettet")
+                                    SVProgressHUD.dismiss(withDelay: 2)
+                                }
+                }
+                
+            }
+            
+            
+            
+            
+           
+           }
+       }
+    }
+    func opretBrugeriFirestore() -> Bool {
+        
+        
+        return true
     }
     func gaaTilMenu() -> Void {
         //TODO: send brugeren videre til tabbar og er nu logget ind. 
